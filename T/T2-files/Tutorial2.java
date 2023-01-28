093a16052a34d7e0dc044bc5de5d11a93ad934c0
Messy Toilet <jacob.chiu168@hotmail.com>
2023-01-28 00:13:45 -0500

    finished T work

--- T/T2-files/Tutorial2.java
+++ T/T2-files/Tutorial2.java
@@ -13,8 +13,11 @@ public class Tutorial2{
       makeList(3,3) -> [3]
    */
    public static int[] makeList(int start, int end){
-      return new int[]{};
-
+      int[] new_list = new int [(end - start) + 1];
+      for (int i = start; i <= end; i++) {
+         new_list[i - start] = i;
+      }
+      return new_list;
    }
  
    /* takes an array of integers and returns an array of arrays
@@ -26,9 +29,16 @@ public class Tutorial2{
        [ [0,1], [0,1,2,3], [0,1,2,3,4,5], [0], [0,1,2,3] ]
     */
    public static int[][] makePlot(int[] data){
-      return new int[][]{};
+      int[][] new_2D_list = new int[data.length][]; 
+      for (int i = 0; i < data.length; i++) {
+         int[] new_list = new int[data[i] + 1];
+         for (int k = 0; k <= data[i]; k++) {
+            new_list[k] = k;
+         }
+         new_2D_list[i] = new_list;
+      }
+      return new_2D_list;
    }
-
 
    /* takes an array of integers and returns an array of arrays
       of those integers except for zeros. 
@@ -40,8 +50,30 @@ public class Tutorial2{
        [ [0,1], [0,1,2,3], [0,1,2,3,4,5], [0,1,2,3] ]
     */
     public static int[][] makePlotWithoutZeros(int[] data){
-      return new int[][]{};
-   }
+      byte counter = 0;
+      for (int i = 0; i < data.length; i++) {   
+         if (data[i] != 0) {
+            counter += 1;
+         }
+      }
+      int[] parsed_array = new int[counter];
+      int[][] final_array = new int[counter][];
+      counter = 0;
+      for (int i = 0; i < data.length; i++){
+         if (data[i] != 0) {
+            parsed_array[counter] = data[i];
+            counter += 1;
+         }
+      }
+      for (int i = 0; i < parsed_array.length; i++) {         
+         int[] passing_array = new int[parsed_array[i] + 1];
+         for (int k = 0; k <= parsed_array[i]; k++) {
+            passing_array[k] = k; 
+         }      
+         final_array[i] = passing_array;
+      }
+      return final_array;
+   } 
 
 
    public static boolean same(int[] a, int[] b){
