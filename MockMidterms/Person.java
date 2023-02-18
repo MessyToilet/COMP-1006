f66ca5f498db0671cf48bff48bbbee8c9aa93473
Messy Toilet <jacob.chiu168@hotmail.com>
2023-02-17 21:38:44 -0500

    midterm stuff

--- MockMidterms/Person.java
+++ MockMidterms/Person.java
@@ -1,5 +1,4 @@
 //  A
-/*
 public class Person {
     public String name     = "";
     public String favPet   = "";
@@ -25,57 +24,24 @@ public class Person {
         }
     }
 
-    
-    
-
-
-    public static void main(String[] args) {
-        Person p = new Person("John", "Cat", new String[]{"Cat", "Cat", "Cat", "Dog", "Cat"});
-        
-
-        System.out.println(p.name);
-        System.out.println(p.SuperHappy());
-
-    }
-}
-*/
-// B
-
-public class Person {
-    public String name     = "";
-    public String favPet   = "";
-    public String[] petLis;
-    
-    Person(String name, String favPet, String[] pets) {
-        this.name = name;
-        this.favPet = favPet;
-        this.petLis = pets;
-    }
-    public boolean SuperHappy() {
-        int count = 0;
-        for (int i = 0; i < this.petLis.length; i++) {
-            if (this.petLis[i] == this.favPet) {
-                count += 1;
+    public static String[][] countPet(String pet, Person[] people) {
+        String[][] countPetLis = new String[people.length][];
+        for (int i = 0; i < people.length; i++) {
+            int count = 0;
+            for (int k = 0; k < people[i].petLis.length; i++) {
+                if (people[i].petLis[k] == pet) {
+                    count += 1;
+                }
             }
+            countPetLis[i][0] = people[i].name;
+            countPetLis[i][1] = Integer.toString(count);
+            count = 0;
         }
-        if (count >= (this.petLis.length + 1) / 2){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return countPetLis;
     }
-
-    
-    
-
-
-    public static void main(String[] args) {
-        Person p = new Person("John", "Cat", new String[]{"Cat", "Cat", "Cat", "Dog", "Cat"});
-        
-
-        System.out.println(p.name);
-        System.out.println(p.SuperHappy());
+    public static void main(String[] args){
 
     }
 }
+
+
---
+++ MockMidterms/QQ.java
@@ -0,0 +1,9 @@
+public class QQ {
+    private final int q;
+
+    public int get_q() {
+        int q = this.q;
+        return q;
+    }
+}
+
