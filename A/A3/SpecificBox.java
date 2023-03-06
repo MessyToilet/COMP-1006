e4b8b7e5797b7d8546931983570094e48d4f12c7
Messy Toilet <jacob.chiu168@hotmail.com>
2023-03-06 16:19:30 -0500

    A3

---
+++ A/A3/SpecificBox.java
@@ -0,0 +1,12 @@
+public class SpecificBox extends Box{
+
+
+   /* ------------------------------------------------------------
+       Please NOTE the ordering of the input parameters
+    -------------------------------------------------------------*/
+
+   public SpecificBox(String label, String location, int size){
+      // add code here
+   }
+
+}
