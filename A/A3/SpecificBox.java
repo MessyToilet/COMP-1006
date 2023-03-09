8d18e01fa50193f7276b2bd1daa2d266d76086bd
Messy Toilet <jacob.chiu168@hotmail.com>
2023-03-08 22:00:05 -0500

    A3 Done

--- A/A3/SpecificBox.java
+++ A/A3/SpecificBox.java
@@ -1,12 +1,40 @@
 public class SpecificBox extends Box{
-
-
    /* ------------------------------------------------------------
        Please NOTE the ordering of the input parameters
     -------------------------------------------------------------*/
-
    public SpecificBox(String label, String location, int size){
       // add code here
-   }
+         super(label, location, size);
+         this.label     = label;
+         this.location  = location;
+         this.size      = size;
+      }
 
+   @Override
+   public int compareTo(Box box1){
+      // System.out.println("");
+      // System.out.println("size     : " + this.size + "\t" + box1.size);
+      // System.out.println("location : " + this.location + "\t" + box1.location);
+      // System.out.println("label    : " + this.label + "\t" + box1.label);
+      int loc = this.location.compareTo(box1.location);
+      if (loc != 0){
+         System.out.println("returning at loc " + loc);
+         return loc / Math.abs(loc);
+      }
+      
+      int lab = Integer.compare(this.label.length(), box1.label.length());
+      if (lab != 0){
+         // System.out.println("returning at lab " + lab);
+         return (lab / Math.abs(lab)) * 2;
+      }
+      
+      int siz = Integer.compare(box1.size, this.size);
+      if (siz != 0){
+         // System.out.println("returning at siz");
+         return (siz / Math.abs(siz)) * 3;
+      }
+      else {
+         return 0;
+      }
+   }
 }
--- test.java
+++ test.java
@@ -1,14 +1,7 @@
 public class test{
       public static void main(String[] args){
-        System.out.println("cat to cat");
-        System.out.println("cat".compareTo("cat"));
-        System.out.println("cat to dog");
-        System.out.println("cat".compareTo("dog"));
-        System.out.println("cat to category");
-        System.out.println("cat".compareTo("category"));
-        System.out.println("category to cat");
-        System.out.println("category".compareTo("cat"));
-        System.out.println("dog to cat");
-        System.out.println("dog".compareTo("cat"));
+        String word1 = "world";
+        String word2 = "xanadu";
+        System.out.println(word1.compareTo(word2));
       }
   }
